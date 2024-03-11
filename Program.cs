cc9da86556a1b31401c693daf3d9a127714062de
Deybson Fellipe <felipecarvalho9673@gmail.com>
2024-03-11 20:03:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using bootcsharp.Models;
+// OPERADORES ARITMÉTICOS EM C# //
+
+
+// Introdução a operadores aritméticos
+// Criando a nossa classe calculadora 
+
+
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+
+
+
+  // Usando potencia 
+
+calc.Potencia(3, 3);
+
+
+
+// Funções trigonométricas
+
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+
+
+
+// Incremento e decremento 
+
+
+int numero = 10;
+
+Console.WriteLine(numero);
+
+Console.WriteLine("Incrementando o 10"); 
+
+numero++;
+
+Console.WriteLine(numero);
+
+
+// Calculando raiz quadrada
+
+calc.RaizQuadrada(9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
